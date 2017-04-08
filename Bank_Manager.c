59389da5061319df37731b4ef540b310d7308ea0
zkate-97 <zkate-97@yandex.ru>
2017-04-09 01:05:15 +0300

    adding file Bank_Manager.c

---
+++ Bank_Manager.c
@@ -0,0 +1,36 @@
+//
+//  main.c
+//  TP_lab5
+//
+//  Created by Katsiaryna Zaitsava on 07.04.17.
+//  Copyright © 2017 Katsiaryna Zaitsava. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sqlite3.h>
+#include <string.h>
+#include "Bank_Manager.h"
+
+char *sql;
+sqlite3 *db = NULL;
+char *err_msg = NULL;
+sqlite3_stmt *res;
+int rc;
+char* oper = "Operator";
+char* admin = "Administrator";
+
+void openDB(){
+    rc = sqlite3_open("Bank_System.db", &db);
+    if (rc != SQLITE_OK) {
+        
+        fprintf(stderr, "Cannot open database: %s\n",
+                sqlite3_errmsg(db));
+        sqlite3_close(db);
+        
+        exit(1);
+    }
+
+}
+
+
